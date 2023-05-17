ab2c4746a5a3a277d5cb3b58d2da539759b6c5b9
adityachakra16 <adityachakra16@gmail.com>
2023-05-17 10:16:15 +0530

    pricing addition

--- src/modules/Pricing/index.tsx
+++ src/modules/Pricing/index.tsx
@@ -1,18 +1,45 @@
 import { CheckOutlined } from "@ant-design/icons";
 import { PinkBlur, VioletBlur, GradientWord } from "../Home/1-Hero-Section";
 
-const featuresArray = [
-  "Unlimited Spaces",
-  "Unlimited Form Responses",
-  "Unlimited Project Cards",
-  "Unlimited Automations",
-  "Infintely Deep Workstreams",
-  "Unlimited Form Plugins",
-  "Granular Roles & Permissions",
-  "Unlimited Web3 Integrations",
-  "Payment with Custom Tokens",
-  "Access to Spect Community",
-  "Priority Support",
+const freePlan = [
+  "Unlimited forms with unlimited responses",
+  "Collect crypto donations on forms",
+  "Paywall forms with tokens",
+  "Advanced form logic",
+  "Hidden form fields",
+  "Role gating forms with Guild.xyz & Discord",
+  "Sybil protection on forms with Gitcoin Passport",
+  "Incentivization & memorabilia with POAP & NFTs",
+  "Prevent bots with captcha",
+  "Custom message on form submission",
+  "Lookup on chain data of form responders",
+  "Collect form responses on Discord",
+  "2 plugins per form",
+  "10 automations",
+  "Unlimited Projects with max 200 rows",
+  "Pre-set roles",
+  "3 members in space",
+];
+
+const paidPlan = [
+  "Everything in Free",
+  "Unlimited form plugins",
+  "Response Analytics",
+  "No Spect branding at the end of form",
+  "200 automations",
+  "Unlimited projects with unlimited rows",
+  "Unlimited workstreams",
+  "5 members in space",
+  "$10 per additional member",
+];
+
+const orgPlan = [
+  "Everything in paid plan",
+  "Custom roles and granular access management",
+  "Priority support",
+  "Personalized pricing",
+  "Custom solutions",
+  "Dedicated success manager",
 ];
 
 export default function Pricing() {
@@ -21,32 +48,119 @@ export default function Pricing() {
       <VioletBlur className="absolute top-0 left-0 blur-xl" />
       <PinkBlur className="absolute right-0 bottom-48 h-1/6 w-1/6 opacity-50" />
       <PinkBlur className="absolute bottom-36 left-72 h-24 w-24" />
-      <div className="flex flex-col text-center items-center z-20">
-        <div className="lg:col-span-1 md:col-span-2 sm:col-span-2 bg-gradientTwo rounded-xl h-full p-8">
-          <h1 className="md:leading-normal sm:leading-normal lg:leading-tight lg:text-4xl md:text-3xl sm:text-3xl font-bold z-40 p-2">
-            For teams of all sizes
-          </h1>
-          <h1 className="md:leading-normal sm:leading-normal lg:leading-tight lg:text-4xl md:text-3xl sm:text-3xl font-bold z-40 p-2">
-            <GradientWord className="my-3">Free</GradientWord>
-          </h1>
-
-          <div className="flex flex-col gap-3 mt-4">
-            {featuresArray.map((feature) => {
-              return (
-                <div key={feature} className="flex flex-row gap-3 items-center">
-                  <CheckOutlined style={{ color: "#BF5AF2" }} />
-                  <p className="text-zinc-400 hover:text-white duration-700">
-                    {feature}
-                  </p>
-                </div>
-              );
-            })}
+      <div className="flex flex-col text-center items-center z-20 gap-8">
+        <div className="flex flex-col text-center items-center z-20">
+          <div className="lg:col-span-1 md:col-span-2 sm:col-span-2 bg-gradientTwo rounded-xl h-full p-8">
+            <h1 className="md:leading-normal sm:leading-normal lg:leading-tight lg:text-4xl md:text-3xl sm:text-3xl font-bold z-40 ">
+              For individual & small teams
+            </h1>
+            <h1 className="md:leading-normal sm:leading-normal lg:leading-tight lg:text-4xl md:text-3xl sm:text-3xl font-bold z-40 p-2">
+              <GradientWord className="my-3">Free</GradientWord>
+            </h1>
+            <div className="flex flex-row gap-3 mt-4">
+              <div className="flex flex-col gap-3 mt-4 w-96">
+                {freePlan.slice(0, 8).map((feature) => {
+                  return (
+                    <div
+                      key={feature}
+                      className="flex flex-row gap-3 items-center text-start"
+                    >
+                      <CheckOutlined style={{ color: "#BF5AF2" }} />
+                      <p className="text-zinc-400 hover:text-white duration-700">
+                        {feature}
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="flex flex-col gap-3 mt-4 w-96">
+                {freePlan.slice(8, 17).map((feature) => {
+                  return (
+                    <div
+                      key={feature}
+                      className="flex flex-row gap-3 items-center"
+                    >
+                      <CheckOutlined style={{ color: "#BF5AF2" }} />
+                      <p className="text-zinc-400 hover:text-white duration-700">
+                        {feature}
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <a href="https://circles.spect.network/">
+              <button className="px-8 py-3 my-3 mt-8 rounded-xl text-md text-purple text-bold bg-purple bg-opacity-5 hover:bg-opacity-25 duration-700">
+                Start Building
+              </button>
+            </a>
           </div>
-          <a href="https://circles.spect.network/">
-            <button className="px-8 py-3 my-3 mt-8 rounded-xl text-md text-purple text-bold bg-purple bg-opacity-5 hover:bg-opacity-25 duration-700">
-              Get Started
-            </button>
-          </a>
+        </div>
+        <div className="flex flex-row text-center items-start z-20 gap-8 h-128">
+          <div className="flex flex-col text-center items-center z-20">
+            <div className="lg:col-span-1 md:col-span-2 sm:col-span-2 bg-gradientTwo rounded-xl h-full p-8">
+              <h1 className="md:leading-normal sm:leading-normal lg:leading-tight lg:text-4xl md:text-3xl sm:text-3xl font-bold z-40 p-2">
+                For larger teams
+              </h1>
+              <h1 className="md:leading-normal sm:leading-normal lg:leading-tight lg:text-4xl md:text-3xl sm:text-3xl font-bold z-40 p-2">
+                <GradientWord className="my-3">$30 </GradientWord>
+              </h1>
+              <div>per month</div>
+
+              <div className="flex flex-col gap-3 mt-4 w-96 text-start">
+                {paidPlan.map((feature) => {
+                  return (
+                    <div
+                      key={feature}
+                      className="flex flex-row gap-3 items-center"
+                    >
+                      <CheckOutlined style={{ color: "#BF5AF2" }} />
+                      <p className="text-zinc-400 hover:text-white duration-700">
+                        {feature}
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+              <a href="https://circles.spect.network/">
+                <button className="px-8 py-3 my-3 mt-8 rounded-xl text-md text-purple text-bold bg-purple bg-opacity-5 hover:bg-opacity-25 duration-700">
+                  Start Building
+                </button>
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col text-center items-center z-20 h-128">
+            <div className="lg:col-span-1 md:col-span-2 sm:col-span-2 bg-gradientTwo rounded-xl h-full p-8">
+              <h1 className="md:leading-normal sm:leading-normal lg:leading-tight lg:text-4xl md:text-3xl sm:text-3xl font-bold z-40 p-2">
+                For organizations
+              </h1>
+              <h1 className="md:leading-normal sm:leading-normal lg:leading-tight lg:text-4xl md:text-3xl sm:text-3xl font-bold z-40 p-2">
+                <GradientWord className="my-3">Custom</GradientWord>
+              </h1>
+              <div>for more than 30 members</div>
+
+              <div className="flex flex-col gap-3 mt-4 w-96 text-start mb-1">
+                {orgPlan.map((feature) => {
+                  return (
+                    <div
+                      key={feature}
+                      className="flex flex-row gap-3 items-center"
+                    >
+                      <CheckOutlined style={{ color: "#BF5AF2" }} />
+                      <p className="text-zinc-400 hover:text-white duration-700">
+                        {feature}
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+              <a href="https://circles.spect.network/">
+                <button className="px-8 py-3 my-3 mt-28 rounded-xl text-md text-purple text-bold bg-purple bg-opacity-5 hover:bg-opacity-25 duration-700">
+                  Get In Touch
+                </button>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
